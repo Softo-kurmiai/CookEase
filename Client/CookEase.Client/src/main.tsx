d0be95ef205c3e692f9795aea84144aabeee610a
Lukas <luk.brasiunas@gmail.com>
2024-04-18 22:13:24 +0300

    Added an API request example in UI using Axios with displaying React toasts

--- Client/CookEase.Client/src/main.tsx
+++ Client/CookEase.Client/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // The React.StrictMode renders components twice which is not ideal in our scenario
+  // but may be used later on
+  //<React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+  </>
+  //</React.StrictMode>,
 )
