f76c19239b70f1b147caf2971d065408a12efceb
GabrieleCiurlionyte <ciurlionyte.gabriele@gmail.com>
2024-04-28 23:58:43 +0300

    Removed unnecessary code

--- Client/CookEase.Client/src/components/HelperComponents/ProfilePage/ProfileTabPanel.tsx
+++ Client/CookEase.Client/src/components/HelperComponents/ProfilePage/ProfileTabPanel.tsx
@@ -17,11 +17,6 @@ export default function ProfileTabPanel() {
 
   return (
     <Box sx={{ width: "80%", margin: "0 auto"}}>
-      <Tabs centered value={value} onChange={handleChange}>
-          <Tab label="Tab 1" />
-          <Tab label="Tab 2" />
-          {/* Add more tabs as needed */}
-        </Tabs>
       <TabContext value={value}>
           <TabList
             onChange={handleChange}
