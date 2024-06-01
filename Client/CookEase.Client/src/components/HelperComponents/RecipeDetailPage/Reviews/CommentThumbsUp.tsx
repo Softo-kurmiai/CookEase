7124a62a72e9c231e3e769b0544dade7584cf095
GabrieleCiurlionyte <ciurlionyte.gabriele@gmail.com>
2024-06-01 23:18:57 +0300

    Added category to recipe details + fixed comment like

--- Client/CookEase.Client/src/components/HelperComponents/RecipeDetailPage/Reviews/CommentThumbsUp.tsx
+++ Client/CookEase.Client/src/components/HelperComponents/RecipeDetailPage/Reviews/CommentThumbsUp.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import IconButton from "@mui/material/IconButton";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import CustomToastContainer from "../../../../utils/Notifications/CustomToastContainer";
-import { showToastError } from "../../../../utils/Notifications/toastUtils.ts";
+import { showToastError } from "../../../../utils/Notifications/toastUtils";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
@@ -19,27 +19,23 @@ const ThumbsUpComponent = ({
   isAuthenticated,
 }: ThumbsUpComponentProps) => {
   const [isLiked, setIsLiked] = useState(false);
-
-  let actionName = "";
-
   const [commentLike, setCommentLike] = useState<number | null>(null);
 
-  React.useEffect(() => {
+  useEffect(() => {
     async function getCommentLike(commentId: number) {
       try {
         const response = await axios.get(
           `/api/comments/comment/${commentId}/totalLikeCount`
         );
         setCommentLike(response.data);
-        console.log("Gotten comment likes:")
-        console.log(commentLike)
+        console.log("Gotten comment likes:", response.data);
       } catch (error) {
         console.error(error);
         setCommentLike(0);
       }
     }
 
-    if (commentId && commentId != 0) {
+    if (commentId && commentId !== 0) {
       getCommentLike(commentId);
     } else if (commentId === 0) {
       setCommentLike(0);
@@ -49,13 +45,8 @@ const ThumbsUpComponent = ({
   const handleThumbsUpClick = async () => {
     console.log("Click");
     if (isAuthenticated) {
-      setIsLiked(!isLiked);
-
-      if (isLiked) {
-        actionName = "Increase";
-      } else {
-        actionName = "Decrease";
-      }
+      const newIsLiked = !isLiked;
+      const actionName = newIsLiked ? "Increase" : "Decrease";
 
       try {
         await axios.put(
@@ -64,11 +55,14 @@ const ThumbsUpComponent = ({
             action: actionName,
           }
         );
+
+        // Update the like count
+        setCommentLike(prev => (prev !== null ? (newIsLiked ? prev + 1 : prev - 1) : prev));
+        setIsLiked(newIsLiked);
         console.log("Call completed");
       } catch (error) {
         console.error("Error liking the comment:", error);
         showToastError("There was an error liking this comment");
-        setIsLiked(isLiked);
       }
     } else {
       showToastError("Log in to like other comments");
@@ -77,7 +71,7 @@ const ThumbsUpComponent = ({
 
   return (
     <IconButton onClick={handleThumbsUpClick}>
-      <CustomToastContainer></CustomToastContainer>
+      <CustomToastContainer />
       <Stack direction="row">
         <ThumbUpIcon
           sx={{
@@ -89,7 +83,7 @@ const ThumbsUpComponent = ({
           sx={{
             textDecoration: "underline",
             color: "info.main",
-            pl: "1rem"
+            pl: "1rem",
           }}
         >
           {commentLike} people found this helpful
