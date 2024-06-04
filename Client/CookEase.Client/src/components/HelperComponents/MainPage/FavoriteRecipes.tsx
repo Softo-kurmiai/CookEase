18c178838dafbceb4ebc33ebcc1ac525659486e8
GabrieleCiurlionyte <ciurlionyte.gabriele@gmail.com>
2024-06-04 21:19:42 +0300

    Filled the favorite and your creators pages

--- Client/CookEase.Client/src/components/HelperComponents/MainPage/FavoriteRecipes.tsx
+++ Client/CookEase.Client/src/components/HelperComponents/MainPage/FavoriteRecipes.tsx
@@ -1,30 +1,44 @@
 import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import { RecipeCard } from '../../MainComponents/RecipeCard';
-import {Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { Typography, useMediaQuery } from "@mui/material";
 import { Theme } from "@mui/material/styles";
+import axios from 'axios';
+import { RecipeData } from '../../../interfaces/RecipeDetailsInterfaces';
 
 export function FavoriteRecipes(){
     const isSmallScreen = useMediaQuery((theme : Theme) => theme.breakpoints.down('sm'));
     const [page, setPage] = React.useState(1);
+    const [recipes, setRecipes] = React.useState([]);
 
     const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
         setPage(value);
-      };
+    };
+
+    React.useEffect(() => {
+        async function getRandomRecipes() {
+            try {
+                const response = await axios.get(`/api/recipes/random?maxNumberOfRecipes=5`);
+                console.log(response.data);
+                setRecipes(response.data);
+            } catch (error) {
+                console.log("Something bad happened during the request!", error);
+            }
+        }
+        getRandomRecipes()
+    }, []);
 
     return (
         <Stack alignItems="center" sx={{
             paddingTop: "0.5rem",
             paddingBottom: "0.5rem"
         }}>
-            <Typography variant={isSmallScreen ? "h6" : "h5"} align="left" sx={{ fontWeight: 600, pb:"0.8rem" }}>Your favorite recipes:</Typography>
+            <Typography variant={isSmallScreen? "h6" : "h5"} align="left" sx={{ fontWeight: 600, pb:"0.8rem" }}>Your favorite recipes:</Typography>
             <Stack spacing={2} direction="row" alignItems="center" justifyContent="center">
-                <RecipeCard isFavorited={true}></RecipeCard>
-                <RecipeCard isFavorited={true}></RecipeCard>
-                <RecipeCard isFavorited={true}></RecipeCard>
-                <RecipeCard isFavorited={true}></RecipeCard>
-                <RecipeCard isFavorited={true}></RecipeCard>
+                {recipes.map((recipe : RecipeData, index: number) => (
+                    <RecipeCard key={index} recipeData={recipe} isFavorited={true} />
+                ))}
             </Stack>
             <Pagination count={10} page={page} onChange={handleChange} color="primary" sx={{
                 padding:"0.5rem"
